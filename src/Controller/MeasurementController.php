e6e2e1f5f6465be28a22dc9da972d5fa49ae7187
Bartek Szczepaniak <32700076+Bartson@users.noreply.github.com>
2022-11-13 19:54:35 +0100

    Add lab5

--- src/Controller/MeasurementController.php
+++ src/Controller/MeasurementController.php
@@ -10,11 +10,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[Route('/measurement')]
+/**
+ * @Route("/measurement")
+ */
 class MeasurementController extends AbstractController
 {
-    #[Route('/', name: 'app_measurement_index', methods: ['GET'])]
+    /**
+     * @Route("/", name="app_measurement_index", methods={"GET"})
+     */
     public function index(MeasurementRepository $measurementRepository): Response
     {
         return $this->render('measurement/index.html.twig', [
@@ -22,11 +27,16 @@ class MeasurementController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_measurement_new', methods: ['GET', 'POST'])]
+    /**
+     * @Route("/new", name="app_measurement_new", methods={"GET", "POST"})
+     * @IsGranted("ROLE_MEASUREMENT_CREATE")
+     */
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $measurement = new Measurement();
-        $form = $this->createForm(MeasurementType::class, $measurement);
+        $form = $this->createForm(MeasurementType::class, $measurement, [
+            'validation_groups' => ['edit', 'new']
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -42,7 +52,9 @@ class MeasurementController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_measurement_show', methods: ['GET'])]
+    /**
+     * @Route("/{id}", name="app_measurement_show", methods={"GET"})
+     */
     public function show(Measurement $measurement): Response
     {
         return $this->render('measurement/show.html.twig', [
@@ -50,10 +62,15 @@ class MeasurementController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_measurement_edit', methods: ['GET', 'POST'])]
+    /**
+     * @Route("/{id}/edit", name="app_measurement_edit", methods={"GET", "POST"})
+     * @IsGranted("ROLE_MEASUREMENT_EDIT")
+     */
     public function edit(Request $request, Measurement $measurement, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(MeasurementType::class, $measurement);
+        $form = $this->createForm(MeasurementType::class, $measurement, [
+            'validation_groups' => ['edit', 'new']
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -68,7 +85,10 @@ class MeasurementController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_measurement_delete', methods: ['POST'])]
+    /**
+     * @Route("/{id}", name="app_measurement_delete", methods={"POST"})
+     * @IsGranted("ROLE_MEASUREMENT_DELETE")
+     */
     public function delete(Request $request, Measurement $measurement, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$measurement->getId(), $request->request->get('_token'))) {
